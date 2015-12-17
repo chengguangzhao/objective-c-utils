67b64bcc5fead16bdac3ffa5f9ed870f98e5c40d
Chengguang Zhao <chengguangzhao@cheng.wireless.yale.internal>
2015-12-16 23:57:54 -0500

    Initial Commit

---
+++ Useful Utilities/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Useful Utilities
+//
+//  Created by Chengguang Zhao on 12/16/15.
+//  Copyright © 2015 Chengguang Zhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
