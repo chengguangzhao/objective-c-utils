11e099e9943453c096d1d44df6b629b7f6939e00
Chengguang Zhao <chengguangzhao@cheng.wireless.yale.internal>
2015-12-27 18:03:56 -0500

    minor

--- Useful Utilities/main.m
+++ Useful Utilities/main.m
@@ -7,33 +7,29 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "MathUtils.h"
+#import "Primes.h"
 
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSDate * start = [NSDate date];
-
-        long double numerators [1000];
-        long double denominators [1000];
-        int nl [1000];
-        int dl [1000];
-        nl[0] = nl[1] = dl[0] = dl[1] = 1;
-        int count = 0;
-        numerators[0] = 3.0;
-        numerators[1] = 7.0;
-        denominators[0] = 2.0;
-        denominators[1] = 5.0;
-        for (int i=2; i<1000; ++i) {
-            denominators[i] = denominators[i-1] + numerators[i-1];
-            numerators[i] = denominators[i] + denominators[i-1];
-            if ( [MathUtils numDigitsLDbl:numerators[i]]> [MathUtils numDigitsLDbl:denominators[i]])
-                ++count;
+        int numDiagonalNumbers = 1;
+        int numDiagonalPrimes = 0;
+        int i = 3;
+        for (; ; i+=2){
+            numDiagonalNumbers += 4;
+            unsigned long long highestD = i * i;
+            for (int j = 0; j < 4; ++j) {
+                if ([Primes isPrime:highestD]) {
+                    ++numDiagonalPrimes;
+                }
+                highestD -= (i-1);
+            }
+            if ((double)numDiagonalPrimes / (double)numDiagonalNumbers < 0.1) break;
         }
 
-        NSLog(@"%d number numberators longer", count);
+        NSLog(@"%d numbers on each side", i);
         NSTimeInterval timeInterval = [start timeIntervalSinceNow];
-
         NSLog(@"%f milliseconds has elapsed.", -timeInterval*1000);
     }
     return 0;
